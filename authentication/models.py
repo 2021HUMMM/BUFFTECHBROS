45c563c4a8462270a421921e775342984aad34a1
Ilham Satya Nusabhakti <ilhamsn2021@gmail.com>
2025-06-27 13:46:00 +0700

    successfully connected to newsAPI

---
+++ authentication/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+
+# necessar imports for authentication models
+from django.contrib.auth.models import User
+
--- main/views.py
+++ main/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
+from newsapi import NewsApiClient
+from django.conf import settings
+
 
 # Create your views here.
 
 def show_main(request):
-    return render(request, 'main.html', {})
+    # Get news data
+    news_data = get_news(request, "bitcoin")
+    # Pass news data to the template
+    context = {
+        'news_data': news_data
+    }
+    return render(request, 'main.html', {'news_data': news_data['articles']})
+
+def get_news(request, topic):
+    newsapi = NewsApiClient(settings.NEWS_API_KEY)
+    # get bahasa indonesia news
+    data = newsapi.get_everything(q=topic, language='en', page_size=5)
+    return data
