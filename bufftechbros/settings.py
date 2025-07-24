4b0f5698bec3e9f91c6a1649566ce3dfac1f614a
Ilham Satya Nusabhakti <ilham.satya@ui.ac.id>
2025-07-24 10:23:32 +0700

    fix: added csrf trusted origin

--- bufftechbros/settings.py
+++ bufftechbros/settings.py
@@ -56,6 +56,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://bufftechbros-production.up.railway.app"
+]
+
 ROOT_URLCONF = 'bufftechbros.urls'
 
 TEMPLATES = [
